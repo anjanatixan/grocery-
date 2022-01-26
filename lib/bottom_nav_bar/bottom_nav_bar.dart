768aa688c88a2d85a3493c46864b5899ab69bc71
anjanatixan <anjanatixan11@gmail.com>
2022-01-26 12:41:31 +0530

    bottom nav bar commit

--- lib/bottom_nav_bar/bottom_nav_bar.dart
+++ lib/bottom_nav_bar/bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery/screens/home.dart';
+import 'package:grocery/screens/password%20.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({ Key? key }) : super(key: key);
@@ -9,179 +10,94 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-   int _selectedItem = 0;
-  //    static const List<Widget> _widgetOptions = <Widget>[
-  //      Home(),
-  //     ];
-  //    void _onItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //   });
-  //  }
-
+   int _selectedIndex = 0;
+     static const List<Widget> screens = [
+       Home(),
+       Password(),
+      
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
  @override
   Widget build(BuildContext context) {
     return Scaffold(
-  //     appBar:  AppBar(
-  //       elevation: 0,
-  //       iconTheme: const IconThemeData(color: Colors.black),
-  //       backgroundColor: Colors.white,
-  //       title: const Text(
-  //         "Grocery Go",
-  //         style: TextStyle(color: Colors.black, fontSize: 20,fontFamily: "Poppins"),
-  //       ),
-  //       actions: <Widget>[
-  //       IconButton(
-  //       icon: Icon(
-  //       Icons.notifications_outlined,
-  //       color: Colors.black,
-  //     ),
-  //     onPressed: (){}, )
-  // ],
-  //     ),
-      
-  //      body: SizedBox(),
-
-//        bottomNavigationBar: BottomNavigationBar(
-//           items: const <BottomNavigationBarItem>[
-//             BottomNavigationBarItem(
-//                 icon: Icon(Icons.home,color: Colors.black),
-//                 title: Text('Home'),
-//                 backgroundColor: Colors.white),
-//             BottomNavigationBarItem(
-//                 icon: Icon(Icons.category_outlined,color: Colors.black),
-//                title: Text('Category'),
-//                 backgroundColor: Colors.white),
-//             BottomNavigationBarItem(
-//                 icon: Icon(Icons.shopping_bag_outlined,color: Colors.black),
-//                title: Text('Cart'),
-//                 backgroundColor: Colors.white),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.menu,color: Colors.black),
-//              title: Text('Menu'),
-//               backgroundColor: Colors.white,
-//             ),
-//           ],
-//           type: BottomNavigationBarType.shifting,
-//           currentIndex: _selectedIndex,
-//           selectedItemColor: Colors.blue,
-//           iconSize: 35,
-//           onTap: _onItemTapped,
-//           elevation: 5),
-//     );
-//   }
-// }
-bottomNavigationBar: CustomBottomNavigationBar(
-        iconList: [
-          Icons.home,
-          Icons.category_outlined,
-          Icons.shopping_bag_outlined,
-          Icons.menu,
-          
-        ],
-        onChange: (val) {
-          setState(() {
-            _selectedItem = val;
-          });
-        },
-        defaultSelectedIndex: 1,
-      ),
-          appBar:  AppBar(
+    appBar:  AppBar(
         elevation: 0,
         iconTheme: const IconThemeData(color: Colors.black),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey[200],
         title: const Text(
           "Grocery Go",
           style: TextStyle(color: Colors.black, fontSize: 20,fontFamily: "Poppins"),
         ),
         actions: <Widget>[
         IconButton(
-        icon: Icon(
+        icon:const Icon(
         Icons.notifications_outlined,
         color: Colors.black,
       ),
       onPressed: (){}, )
     ],
       ),
-      // body: Center(
-      //   child: Text(
-      //     "Hello from Item $_selectedItem",
-      //     style: TextStyle(fontSize: 26),
-      //   ),
-      // ),
-    );
-  }
-}
-
-class CustomBottomNavigationBar extends StatefulWidget {
-  final int defaultSelectedIndex;
-  final Function(int) onChange;
-  final List<IconData> iconList;
-
-  CustomBottomNavigationBar(
-      {this.defaultSelectedIndex = 0,
-      required this.iconList,
-      required this.onChange});
-
-  @override
-  _CustomBottomNavigationBarState createState() =>
-      _CustomBottomNavigationBarState();
-}
-
-class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
-  int _selectedIndex = 0;
-  List<IconData> _iconList = [];
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    _selectedIndex = widget.defaultSelectedIndex;
-    _iconList = widget.iconList;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> _navBarItemList = [];
-
-    for (var i = 0; i < _iconList.length; i++) {
-      _navBarItemList.add(buildNavBarItem(_iconList[i], i));
-    }
-
-    return Row(
-      children: _navBarItemList,
-    );
-  }
-
-  Widget buildNavBarItem(IconData icon, int index) {
-    return GestureDetector(
-      onTap: () {
-        widget.onChange(index);
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      child: Container(
-        height: 60,
-        width: MediaQuery.of(context).size.width / _iconList.length,
-        decoration: index == _selectedIndex
-            ? BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(width: 4, color: Colors.green),
-                ),
-                gradient: LinearGradient(colors: [
-                  Colors.green.withOpacity(0.3),
-                  Colors.green.withOpacity(0.015),
-                ], begin: Alignment.bottomCenter, end: Alignment.topCenter)
-                // color: index == _selectedItemIndex ? Colors.green : Colors.white,
-                )
-            : BoxDecoration(),
-        child: Icon(
-          icon,
-          color: index == _selectedIndex ? Colors.black : Colors.grey,
-        ),
+       
+bottomNavigationBar: SizedBox(height: 75,
+  child:   Container(
+    decoration: BoxDecoration(
+      color: Colors.grey[100],
+      borderRadius:const BorderRadius.only(
+        topLeft: Radius.circular(40),
+        topRight: Radius.circular(40),
       ),
+      boxShadow:const [
+        BoxShadow(
+          offset: Offset(0,0),
+          blurRadius: 3,
+          color: Colors.grey
+        )
+      ]
+    ),
+    child: BottomNavigationBar(
+    
+              items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                    icon: Icon(Icons.home,),
+                    title: Text('Home'),
+                    backgroundColor: Colors.white38
+                    ),
+                      BottomNavigationBarItem(
+                     icon: Icon(Icons.view_list_rounded),
+                     title: Text('Orders'),
+                     backgroundColor: Colors.white38
+                     ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.category),
+                        title: Text('Products'),
+                        backgroundColor: Colors.white38
+                        ),
+                      BottomNavigationBarItem(
+                      icon: Icon(Icons.person),
+                      title: Text('Profile'),
+                      backgroundColor: Colors.white38,
+                        ),
+                        ],
+    
+              type: BottomNavigationBarType.shifting,
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.grey,iconSize:25,
+              onTap: _onItemTapped,
+              elevation: 50),
+  ),
+),
+       body: Container(
+            color: Colors.grey[300],
+            child: Center(
+              child: screens.elementAt(_selectedIndex),
+            ),
+          ),
+     
     );
   }
 }
+
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,15 +8,112 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+   List<Map<String,String>> imageList = [
+     { "img":"assets/images/Fruits-veg.png",
+      "name":"Fruits& Vegetables",},
+      {"img":"assets/images/breakfast.png",
+      "name":"Breakfast" },
+     {"img":"assets/images/beverages.png",
+      "name":"Beverages"},
+      { "img":"assets/images/meat-fish.png",
+      "name":"Meat & Fish",},
+      { "img":"assets/images/diary.png",
+      "name":"Dairy",},
+      { "img":"assets/images/snacks.png",
+      "name":"Snacks",}
+      ];
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Container(
       
-      body: Column(
-        children: [
-          Text("ghh"),
-        ],
-      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+            children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                      shape:BoxShape.circle ,color: Colors.blue,),
+                      child: IconButton(onPressed: (){}, icon: Icon(Icons.location_on_outlined,color: Colors.white,),
+                     iconSize: 20.0,),
+                    ),
+                 SizedBox(width: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:const [
+                Text("Your Location",style: TextStyle(fontFamily: 'Poppins'),),
+                Text("3rd Street, MG Road,Kochi,Kerala",style: TextStyle(fontFamily: 'Poppins'))
+              ],)
+                ],
+               ),
+            ),
+            Container(
+              margin: EdgeInsets.all(12),
+              child: TextField(
+                decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide.none
+                          ),
+                          filled: true,
+                          fillColor: Colors.white70,
+                          prefixIcon: Icon(Icons.search,color: Colors.black),
+                          hintText: "Search Anything",hintStyle: TextStyle(fontFamily: 'Poppins',),
+                           ),
+              ),
+            ),
+
+            Expanded(child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 0,
+              crossAxisSpacing: 1), 
+              itemCount: imageList.length,
+              
+              itemBuilder:(context,index){
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    
+                    onTap: (){},
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Container(
+                           
+                         //padding: EdgeInsets.all(5),
+                          alignment: Alignment.bottomCenter,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                              )
+                            ],
+                          borderRadius: BorderRadius.circular(10),
+                          image:DecorationImage(
+                          image:AssetImage(imageList[index]['img'].toString()),
+                         // fit: BoxFit.cover,
+                          ),
+                          ),
+                          
+                          child:Text(imageList[index]["name"].toString()),
+                           ),
+                      ),
+                    ),
+                  ),
+                );
+              })
+           
+             ) ],
+        ),
+      )
     );
   }
 }
